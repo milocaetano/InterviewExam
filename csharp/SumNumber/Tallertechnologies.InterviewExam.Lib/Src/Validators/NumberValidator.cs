21c59c62883a282503dc345be878a383e9a6c85b
Camilo Caetano <camilo.s.caetano@gmail.com>
2022-02-04 16:18:46 -0300

    validate numeric

--- csharp/SumNumber/Tallertechnologies.InterviewExam.Lib/Src/Validators/NumberValidator.cs
+++ csharp/SumNumber/Tallertechnologies.InterviewExam.Lib/Src/Validators/NumberValidator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using Tallertechnologies.InterviewExam.Lib.Src.Exceptions;
 using Tallertechnologies.InterviewExam.Lib.Src.Interfaces;
 
@@ -26,6 +27,18 @@ namespace Tallertechnologies.InterviewExam.Lib.Src.Validators
             if (String.IsNullOrEmpty(this.value))
             {
                 string msg = $"{this.paramterName} cannot be Null of Empty";
+
+                throw new CalculatorException(msg);
+            }
+
+            this.validateIsNumeric();
+        }
+
+        private void validateIsNumeric()
+        {
+            if (Regex.IsMatch(this.value, @"^\d+$"))
+            {
+                string msg = $"{this.paramterName} must be Numeric";
 
                 throw new CalculatorException(msg);
             }
